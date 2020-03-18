8afdc3fd9ed1178908d6d20d7fda3c078676b3ce
andreipit <andrei.pit@icloud.com>
2020-03-18 19:44:14 +0300

    Add canvas scratch: cells, store and scores

---
+++ Assets/Scripts/Logic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logic : MonoBehaviour
+{
+    public GameObject store;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleStore(bool show)
+    {
+        store.SetActive(show);
+        if (show)
+        {
+            Debug.Log("open store");
+        }
+        else
+        {
+            Debug.Log("close store");
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using CellsDragNDrop;
+
+namespace CellsDragNDrop
+{
+    public class Spawner : MonoBehaviour
+    {
+        //config
+        public GameObject prefab;
+        public Transform[] spawnPoints;
+        public int maxAlive = 1;
+
+        //state
+        public List<GameObject> alive;
+
+        //bookkeeping
+        float lastRandom;
+
+        void Update()
+        {
+            alive = alive.Where(item => item != null).ToList();
+            if (alive.Count < maxAlive) Spawn();
+        }
+
+        void Spawn()
+        {
+            alive.Add(Instantiate(prefab, spawnPoints[GetNewRandom()].position, Quaternion.identity, transform));
+        }
+
+        int GetNewRandom()
+        {
+            int random = Random.Range(0, spawnPoints.Length);
+            while (random == lastRandom) random = Random.Range(0, spawnPoints.Length);
+            lastRandom = random;
+            return random;
+        }
+    }
+}
